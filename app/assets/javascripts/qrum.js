861e6a733470fe9120dcddb04ab67f9bef9ecc55
Mattias Petter Johansson <mpj@mpj.me>
2011-10-29 02:55:42 +0200

    assets

---
+++ app/assets/javascripts/qrum.js
@@ -0,0 +1,11 @@
+$("#mainform form").submit(function(event) {
+	var body = $(".field.body textarea").val();
+	var sig = $(".field.signature input").val();
+	if (body.trim().length == 0) {
+		event.preventDefault();
+		alert("You must enter message body.")
+	} else if (sig.trim().length == 0) {
+		event.preventDefault();
+		alert("You must enter a signature.")
+	}
+});
