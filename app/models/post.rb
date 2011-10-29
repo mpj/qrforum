797726939cbc952341e6b45d3bac01528b346bc7
Mattias Petter Johansson <mpj@mpj.me>
2011-10-29 04:44:16 +0200

    implemented fonts and pretty PDF stuff

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,4 @@ class Post < ActiveRecord::Base
 	belongs_to :wall
 	validates :body, :presence => true, :length => { :maximum => 400 }
 	validates :signature, :presence => true, :length => { :maximum => 25 }
-
 end
